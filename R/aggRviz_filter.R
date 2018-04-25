e278e8179a622380ff02480e02ac6ee551cc9cd8
Amy Goldlist <amygoldlist@gmail.com>
2018-04-25 12:03:55 -0700

    fixed another tiny bug

--- R/aggRviz_filter.R
+++ R/aggRviz_filter.R
@@ -14,8 +14,8 @@
 #' @export
 #'
 #' @examples
-#' aggRviz(dat = my_df, col_2_delete = c("Gender", "Age"))
-#' returns a data frame without stratification by gender or age
+#' aggRviz_filter(dat = my_df, col_2_delete = c("Gender", "Age"))
+#' returns data.frame without stratification by gender or age
 #' but stratified by all other features.
 #'
 #'

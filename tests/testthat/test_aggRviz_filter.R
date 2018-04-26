892a61173ee48f3354e41322dfc91e3700b63325
Amy Goldlist <amygoldlist@gmail.com>
2018-04-26 14:55:12 -0700

    started unit tests for aggRviz_filter

---
+++ tests/testthat/test_aggRviz_filter.R
@@ -0,0 +1,44 @@
+
+context("Basic functionality tests")
+
+
+df_noblanks <- data.frame(
+  g1 = c(1, 1, 2, 2, 2),
+  g2 = c(1, 2, 1, 2, 1),
+  a = c("a", "b", "c", "d", "e"),
+  b = c("q","w","e","r","t")
+)
+
+df_blanks <- data.frame(
+  g1 = c("1", "", "2", "2", "2"),
+  g2 = c("1", "2", "1", "2", "1"),
+  a = c("a", "b", "c", "d", "e"),
+  b = c("q","w","e","","t")
+)
+
+df_blanks_filtered <- data.frame(
+  g1 = c("1",  "2",  "2"),
+  g2 = c("1",  '1',  "1"),
+  a = c("a", "c",  "e"),
+  b = c("q","e","t")
+)
+
+df_empty <- dplyr::select(dplyr::filter(df_blanks,g1 == "aggRviz"),b, g2)
+
+
+
+
+
+
+
+test_that('aggRviz_filter basic functionality', {
+
+  expect_is(aggRviz_filter(df_noblanks, c("g1","a")),'data.frame')
+  expect_is(aggRviz_filter(df_blanks, c("g2","b")),'data.frame')
+  #expect_equivalent(aggRviz_filter(df_noblanks, c("g1","a")), df_empty)
+  #expect_equivalent(filter_blanks(df_blanks), df_blanks_filtered)
+
+  })
+
+
+

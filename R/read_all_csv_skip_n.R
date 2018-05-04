8fe63d0677190e3d0101ff53b0637440516f43f5
Amy Goldlist <amygoldlist@gmail.com>
2018-05-04 11:55:30 -0700

    roxygen skeletopn for read_all_csv_skip_n

--- R/read_all_csv_skip_n.R
+++ R/read_all_csv_skip_n.R
@@ -6,10 +6,25 @@
 ###########
 
 
+#' read_all_csv_skip_n
+#'
+#' This function is used to read all csv in a folder
+#' It return a list of all datasets.
+#' examples: datalist <- read_all_csv_skip_n(path)
+#' The first csv is datalist[[1]]
+#'
+#' @param path
+#' @param n
+#' @param pattern
+#'
+#' @return list
+#' @export
+#'
+#' @examples
 read_all_csv_skip_n<- function(path,n=2, pattern = "*.csv") {
-  
+
   skipn <- function(x,y=n) read.csv(x,skip = y)
-  
+
   files = list.files(path, pattern, full.names = TRUE)
   lapply(files, skipn)
 }

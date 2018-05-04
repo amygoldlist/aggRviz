6b6e8ad2508f95b5303febd8d9123d4fb02b89c4
Fang Yang <yffarris@gmail.com>
2018-05-03 19:05:59 -0700

    read_all function

---
+++ R/read_all_csv_skip_n.R
@@ -0,0 +1,15 @@
+###########
+# This function is used to read all csv in a folder
+# It return a list of all datasets.
+# examples: datalist <- read_all_csv_skip_n(path)
+# The first csv is datalist[[1]]
+###########
+
+
+read_all_csv_skip_n<- function(path,n=2, pattern = "*.csv") {
+  
+  skipn <- function(x,y=n) read.csv(x,skip = y)
+  
+  files = list.files(path, pattern, full.names = TRUE)
+  lapply(files, skipn)
+}

de9993dab559ce17dc16187fd21f2199405dd336
Fang Yang <yffarris@gmail.com>
2018-05-03 19:05:36 -0700

    kprot cluster function

---
+++ R/kprot_cluster.R
@@ -0,0 +1,36 @@
+################
+# This function do k-prototypes cluster for 2 lowest datasets.
+# Since each metric only contain 1 key feature.
+# This function will identify two key features for the two dataset you input,
+# and plot clusering map with two key features as x,y axis.
+################
+
+
+
+# dat1, dat2 should be the clean dataset with no blank.
+# n is the cluster number
+kprot_cluster <- function(dat1,dat2,n){
+  #join two dataset
+  test <- join_set(dat1,dat2)
+  
+  if (nrow(test)!=0){
+  
+  #k-prototype cluster
+  kp <- kproto(test,n)
+  
+  #identify key features
+  key <- c()
+  for (i in 1:length(names(test))){
+    if (T %in% stringr::str_detect(names(test)[i],c("measure","rate")) == T){
+      key = c(key,i)
+    }
+  }
+  
+  #plot cluster map
+  
+  return(plot(test[,key], col = kp$cluster))
+  }
+  else{
+    print("lowest level data set has 0 row")
+  }
+}

9247ac42e51a965112fbdd7261f9b350839754ee
Fang Yang <yffarris@gmail.com>
2018-05-08 17:24:38 -0700

    get_measures function

---
+++ R/get_measures.R
@@ -0,0 +1,36 @@
+get_measures <- function(clean_dataset_path){
+  full_dataset <- read.csv(clean_dataset_path)
+  
+  good_data <- full_dataset %>% 
+    # filter(Trend.quality1=="Green",
+    #        Trend.quality2=="Green",
+    #        Trend.quality3=="Green",
+    #        Trend.quality4=="Green",
+    #        Trend.quality5=="Green",
+    #        Trend.quality6=="Green",
+    #        Trend.quality7=="Green",
+    #        Trend.quality8=="Green",
+    #        Trend.quality9=="Green",) %>% 
+    dplyr::select(Age,
+                  Job.Function..EEOC., 
+                  People.manager,
+                  Performance.group,
+                  Industry,
+                  Tenure,
+                  Country,
+                  Region,
+                  State.or.Province,
+                  Time,
+                  Gender,
+                  gender_diversity_measure,
+                  #headcount_Age_measure,
+                  #headcount_Job.Function..EEOC._measure,
+                  #headcount_People.manager_measure,
+                  high_performers_ratio_measure,
+                  managers_ratio_measure,
+                  wfa_eeoc_minority_ratio_measure,
+                  wfa_promotion_received_rate,
+                  wfa_retention_turnover_resignation_n_rate) %>% 
+    na.omit()
+  return(good_data)
+}

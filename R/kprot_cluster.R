29e771ad8a6ae0e28ea0dcf625be602d5337e405
Fang Yang <yffarris@gmail.com>
2018-05-17 11:13:38 -0700

    update kproto

--- R/kprot_cluster.R
+++ R/kprot_cluster.R
@@ -26,20 +26,43 @@ kprot_cluster <- function(dat1,dat2,n){
   
   if (nrow(test) >= n){
     
-    #k-prototype cluster
-    kp <- clustMixType::kproto(test,n)
+    metrics <- identify_measures(names(test))
     
-    #identify key features
-    key <- c()
-    for (i in 1:length(names(test))){
-      if (T %in% stringr::str_detect(names(test)[i],c("measure","rate")) == T){
-        key = c(key,i)
-      }
+    features <- c()
+    for (i in names(test)){
+      if (i %in% metrics){}
+      else{features = c(features,i)}
     }
+    
+    aaa <- features[! features %in% "Time"]
+    
+    
+    
+    test2 <- aggregate(test %>% dplyr::select(metrics), 
+                       test %>% dplyr::select(aaa), 
+                       mean) 
+    
+    kp <- kproto(test2,n)
+    
+    cluster_plot <- ggplot(test2,aes(x = test2[,length(test2)-1], 
+                                     y = test2[,length(test2)]))+
+      geom_point(aes(color = as.factor(kp$cluster)),palette="Set1",size=2)+
+      geom_point(kp$centers,
+                 mapping = aes(x = kp$centers[,length(kp$centers)-1], 
+                               y =kp$centers[,length(kp$centers)],
+                               color = as.factor(1:range(nrow(kp$centers)))),
+                 palette="Set1",
+                 size = 5,
+                 shape = 21,stroke = 4)+
+      xlab(metrics[1])+
+      ylab(metrics[2])+
+      labs(color="Cluster")
+    
+    cluster_center <- kp$centers
     
     #plot cluster map
     
-    return(graphics::plot(test[,key], col = kp$cluster))
+    return(list(cluster_plot,cluster_center))
   }
   else{
     print(paste0("The row number of the lowest level dataset is less than ",n))

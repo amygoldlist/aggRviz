7221fc7dd44f7819678634ede44e50fd7ed58c70
Amy Goldlist <amygoldlist@gmail.com>
2018-06-06 11:32:47 -0700

    places complete?

--- R/aggRviz_filter2.R
+++ R/aggRviz_filter2.R
@@ -31,7 +31,7 @@
 #' aggRviz_filter2(data = dat_1,col_2_delete= c("Dessert", "Fruit"))
 #'
 #'
-aggRviz_filter2 <- function(data,col_2_delete = NULL, col_2_keep = NULL, features = NULL, all_symbol = "", fix_places = TRUE){
+aggRviz_filter2 <- function(data,col_2_delete = NULL, col_2_keep = NULL, features = NULL, all_symbol = "", fix_place = TRUE){
   if (!is.data.frame(data)){
     stop("Error: data should be a dataframe!")
   }
@@ -44,6 +44,11 @@ aggRviz_filter2 <- function(data,col_2_delete = NULL, col_2_keep = NULL, feature
   if (!is.null(col_2_delete) & !is.null(col_2_keep)){
     stop("Error: enter exactly one of col_2_delete and col_2_keep!")
   }
+
+  if (!is.logical(fix_place) ){
+    stop("Error: fix_place needs to be TRUE or FALSE")
+  }
+
 
   ### checks for features:
 
@@ -65,9 +70,8 @@ aggRviz_filter2 <- function(data,col_2_delete = NULL, col_2_keep = NULL, feature
     }
 
     col_2_delete <- dplyr::intersect(col_2_delete, col_features)
-
     keepers <- dplyr::setdiff(col_features,col_2_delete)
-  }
+      }
 
 ### if we're looking at keeping columns.
   if (is.null(col_2_delete)){
@@ -75,13 +79,41 @@ aggRviz_filter2 <- function(data,col_2_delete = NULL, col_2_keep = NULL, feature
       stop("Error! col_2_keep needs to be a vector!")
     }
 
-
     keepers <- dplyr::intersect(col_2_keep, col_features)
     col_2_delete <- dplyr::setdiff(col_features, keepers)
   }
 
 
   dat <- data
+
+  ###############
+  ## clean up the places, to the lowest level:
+
+  if (fix_place){
+    places <- names(dat) %>%
+      dplyr::intersect(c("State.or.Province", "Region", "Country"))
+
+    if ("State.or.Province" %in% keepers){
+      if ("Region" %in% places & "Region" %in% col_2_delete ){
+        dat <- dat %>%
+          dplyr::select(-Region)
+      }
+      if ("Country" %in% places & "Country" %in% col_2_delete ){
+        dat <- dat %>%
+          dplyr::select(-Country)
+      }
+    } else if ("Region" %in% keepers){
+      if ("Country" %in% places & "Country" %in% col_2_delete ){
+        dat <- dat %>%
+          dplyr::select(-Country)
+      }
+    }
+
+    col_2_delete <- col_2_delete %>%
+      dplyr::intersect(names(dat))
+  }
+
+
 
   ###########  Filtering out all features!
   if(length(keepers)== 0){
@@ -93,25 +125,14 @@ aggRviz_filter2 <- function(data,col_2_delete = NULL, col_2_keep = NULL, feature
   }
 
 
-  #### fix the placenames (project specific for now)
-  if ("Region" %in% keepers){
-    if ("Country" %in% names(dat)){
-      dat <- dat %>%
-        ## make sure country and Region agree
-        dplyr::filter((Country == "" & Region == "") | (Country != "" & Region != "")) %>%
-        dplyr::select(-Country)
-    }
-  }
-  if ("State.or.Province" %in% keepers){
+  #######################
+  ## if there's nothing left to delete:
+  if (length(col_2_delete)==0){
     dat <- dat %>%
-      ## make sure region and province agree
-      dplyr::filter((State.or.Province == "" & Region == "") | (State.or.Province != "" & Region != "")) %>%
-      dplyr::select(-Region)
+      filter_blanks(features, all_symbol) %>%
+      droplevels()
+    return(dat)
   }
-
-  #### get col_2_delete cleaner:
-
-  col_2_delete <- dplyr::intersect(names(dat), col_2_delete)
 
 
   #####################
@@ -125,17 +146,8 @@ aggRviz_filter2 <- function(data,col_2_delete = NULL, col_2_keep = NULL, feature
   dat <- filter_blanks(dat, features, all_symbol)
   dat <- droplevels(dat)
 
+
   return(dat)
 }
-
-
-
-#dat <- data %>%
- # dplyr::filter_at(col_2_delete, dplyr::all_vars(. == all_symbol)) %>%
-  # dplyr::select(-dplyr::one_of(col_2_delete))
-
-
-
-
 
 

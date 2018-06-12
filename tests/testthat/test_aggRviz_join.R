61cf4a372d1837dfc7ce9cc7a2595338c2ba2de7
Amy Goldlist <amygoldlist@gmail.com>
2018-06-12 13:19:51 -0700

    changed to tests

--- tests/testthat/test_aggRviz_join.R
+++ tests/testthat/test_aggRviz_join.R
@@ -24,12 +24,3 @@ test_that('AggRviz_join basic functionality', {
 })
 
 
-test_that('join_set basic functionality', {
-  expect_equal(join_set(dat_1,dat_2), df)
-  expect_is(join_set(dat_1,dat_2),'data.frame')
-  #expect_is(filter_blanks(df_blanks),'data.frame')
-  #expect_equal(filter_blanks(df_noblanks), df_noblanks)
-  #expect_equivalent(filter_blanks(df_blanks), df_blanks_filtered)
-  #expect_error(filter_blanks(5), "data should be a dataframe!")
-  #expect_error(filter_blanks(c(4,65)), "Error: data should be a dataframe!")
-})
